f410dbbf5692bffd2d0cdc52bb5ec0f4aa0b4bba
cheng.cui@etocrm.com <cheng.cui@etocrm.com>
2018-12-19 16:17:22 +0800

    上传文件

---
+++ AuthTips/Common/FileHelper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthTips.Common
+{
+    public class FileHelper
+    {
+        /// <summary>
+        /// 获取目录权限列表
+        /// </summary>
+        /// <param name="path">目录的路径。</param>
+        /// <returns>指示目录的权限列表</returns>
+        public IList<FileSystemRights> GetDirectoryPermission(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                    return null;
+
+                IList<FileSystemRights> result = new List<FileSystemRights>();
+                var dSecurity = Directory.GetAccessControl(new DirectoryInfo(path).FullName);
+                foreach (FileSystemAccessRule rule in dSecurity.GetAccessRules(true, true, typeof(NTAccount)))
+                    result.Add(rule.FileSystemRights);
+
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e);
+            }
+        }
+        /// <summary>
+        /// 判断目录是否包含修改权限
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public bool IsCanModity(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                var fileName = Path.Combine(path, "test01.txt");
+
+                //创建文件
+                FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate);
+                using (StreamWriter sw = new StreamWriter(fs)) // 创建写入流
+                {
+                    sw.WriteLine($"检测时间：{DateTime.Now}");
+                }
+                return true;
+                //var dSecurity = Directory.GetAccessControl(new DirectoryInfo(path).FullName);
+                //foreach (FileSystemAccessRule rule in dSecurity.GetAccessRules(true, true, typeof(NTAccount)))
+                //{
+                //    var right = rule.FileSystemRights;
+
+                //    if (right == FileSystemRights.FullControl  ///所有权限
+                //        || right.ToString().IndexOf("Modify") >= 0  //修改权限
+                //        || right.ToString().IndexOf("Create") >= 0  //创建权限
+                //        || right.ToString().IndexOf("Change") >= 0 //修改文件夹权限
+                //        || right.ToString()=="262144")  //修改文件夹权限
+                //    {
+                //        return true;
+                //    }
+
+                //}
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AuthTips/Program.cs
@@ -0,0 +1,95 @@
+﻿using AuthTips.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using System.Threading;
+using System.Configuration;
+
+namespace AuthTips
+{
+    class Program
+    {
+        static FileHelper fileHelper = new FileHelper();
+        private static Logger log = LogManager.GetCurrentClassLogger();
+        private static bool IsSendEmail = false;
+        private static DateTime SendTime = DateTime.Now;
+        private static bool IsPrint = false;
+        private static DateTime PrintTime = DateTime.Now;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("是否要启动文件夹检测程序？(Y/N)");
+            var responseVal = Console.ReadLine();
+            if (responseVal.ToUpper() == "Y")
+            {
+                Console.WriteLine("开始启动网站文件夹检测；");
+                var filePathStr = ConfigurationManager.AppSettings["path"];
+                var pathList = filePathStr.Split(',');
+                foreach (var filePath in pathList)
+                {
+                    Console.WriteLine(string.Format("检测地址：{0}", filePath));
+                }
+
+                while (true)
+                {
+                    var printVals = new List<string>();
+                    foreach (var path in pathList)
+                    {
+                        var result = fileHelper.IsCanModity(path);
+                        if (result)
+                        {
+
+                            printVals.Add($"检测到文件夹【{path}】权限未关闭，已发送邮件！！！");
+                            if (!IsSendEmail)
+                            {
+                                log.Info($"检测到文件夹【{path}】权限未关闭");
+                                IsSendEmail = true;
+                                IsPrint = false;
+                                SendTime = DateTime.Now;
+                            }
+                        }
+                        else
+                        {
+                            printVals.Add($"已检测文件夹【{path}】权限已关闭");
+                        }
+
+                    }
+                    if (!IsPrint)
+                    {
+                        foreach (var item in printVals)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        IsPrint = true;
+                        PrintTime = DateTime.Now;
+                        
+
+                    }
+                    Task.Factory.StartNew(() =>
+                    {
+                        CountDown();
+                    });
+                    Thread.Sleep(10000);
+
+                }
+            }
+
+
+        }
+        private static void CountDown()
+        {
+            if (SendTime.AddHours(1) <= DateTime.Now && IsSendEmail)
+            {
+                IsSendEmail = false;
+            }
+            if (PrintTime.AddMinutes(10) <= DateTime.Now)
+            {
+                IsPrint = false;
+            }
+        }
+
+
+    }
+}
